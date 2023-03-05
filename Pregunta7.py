fff3f39f78deebda171da285d78cb6a378f7f6c5
Adaniela529 <126400359+Adaniela529@users.noreply.github.com>
2023-03-05 08:31:15 -0500

    Create Pregunta7.py

---
+++ Pregunta7.py
@@ -0,0 +1,12 @@
+Var1=float(input("Ingrese el primer numero: "))
+Var2=float(input("Ingrese el segundo numero: "))
+
+if Var1==Var2:
+    print("Los numeros son iguales")
+elif Var1!=Var2:
+    if Var1>Var2:
+        print("Los numeros son diferentes y el primer valor es mayor que el segundo valor")
+    elif Var1 <= Var2:
+        print("Los numeros son diferentes y el segundo valor es mayor que el primero valor")
+
+ 
