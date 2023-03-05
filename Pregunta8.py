5fdeafc0744b7aff4d595e4b812c182c7f33809b
Adaniela529 <126400359+Adaniela529@users.noreply.github.com>
2023-03-05 08:31:54 -0500

    Create Pregunta8.py

---
+++ Pregunta8.py
@@ -0,0 +1,7 @@
+Contra1="ABC2023"
+Contra2=input("Ingrese contraseña: ")
+
+if Contra1.lower()==Contra2.lower():
+    print("Contraseña correcta")
+elif Contra1.lower() != Contra2.lower():
+        print("Contraseña incorrecta")
