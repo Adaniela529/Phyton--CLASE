2d0ef056ba338c3d2c47142f8098b4980fc45908
Adaniela529 <126400359+Adaniela529@users.noreply.github.com>
2023-03-05 09:38:24 -0500

    Create Pregunta9.py

---
+++ Pregunta9.py
@@ -0,0 +1,11 @@
+Lista1=[('Maria',15,'78562415'),('Juan',56,'65324862'),('Miguel',25,'46952163'),('Gaby',63,'07865492')]
+
+nom=input("ingrese su Nombre: ")
+edad=input("ingrese su edad: ")
+dni=int(input("ingrese su dni: "))
+
+listavacia=[]
+if  listavacia in Lista1:
+    if edad >= 18:
+        Lista1.append((tuple)) 
+        print(tuple(0))
