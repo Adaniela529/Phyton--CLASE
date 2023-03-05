605abcf10558cb4fce902c934552972e926bd51f
Adaniela529 <126400359+Adaniela529@users.noreply.github.com>
2023-03-05 08:29:28 -0500

    Create Pregunta3.py

---
+++ Pregunta3.py
@@ -0,0 +1,14 @@
+PrimerV=float(input("Ingrese Primer Valor: "))
+SegundoV=float(input("Ingrese Segundo Valor: "))
+TercerV=float(input("Ingrese Tercer Valor: "))
+
+Suma=PrimerV+SegundoV+TercerV
+Resta=PrimerV-SegundoV-TercerV
+Multi=PrimerV*SegundoV*TercerV
+Div=PrimerV/SegundoV/TercerV
+DivEntera=PrimerV//SegundoV//TercerV
+print(f"La Suma de los valores es: {Suma}")
+print(f"La Resta de los valores es: {Resta}")
+print(f"La Multiplicación de los valores es: {Multi}")
+print(f"La División de los valores es: {Div}")
+print(f"La División Entera de los valores es: {DivEntera}")
